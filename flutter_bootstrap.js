e20bdb2ca1cc33e16b574a8295145b94a22eb1c7
jowher@2003 <jowhermohammed03@gmail.com>
2025-05-07 01:54:21 +0530

    rebuild site

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2318772102"
+    serviceWorkerVersion: "3031503989"
   }
 });
